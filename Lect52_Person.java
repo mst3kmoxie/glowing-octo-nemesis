01968baf6dde1982482b1d1a45b99fd2fea2876f
Dawn Carroll <dsc254@gmail.com>
2016-04-01 11:40:44 +0100

    Create Lect52_Person.java

---
+++ Lect52_Person.java
@@ -0,0 +1,25 @@
+
+public class Lect52_Person {
+
+	private String name;
+	
+	public Lect52_Person(String name) {
+		this.name = name;
+		
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Lect52_Person [Name = " + name + "]";
+	}
+	
+	
+}
