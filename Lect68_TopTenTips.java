df4e74c0d3b8a0e2968728a3be5f8323f8d9f0d9
Dawn Carroll <dsc254@gmail.com>
2016-04-22 12:20:54 +0100

    Create Lect68_TopTenTips.java

---
+++ Lect68_TopTenTips.java
@@ -0,0 +1,36 @@
+public class Lect68_TopTenTips {
+
+	/*
+	 * Ten Tips To Make You A Better Programmer:
+	 * 
+	 * 10. Learn to touch type: become BFFs with Mavis Beacon.
+	 * 
+	 * 9. Name variables and subroutines descriptively: You should know what
+	 * your variables and methods do just by looking at their names.
+	 * 
+	 * 8. Type rather than read: play with code, don't just read about it.
+	 * 
+	 * 7. Write software that interests you: best way to learn is DO IT.
+	 * 
+	 * 6. Read stack traces from the top line down: the real information in an
+	 * error trace is in the top lines.
+	 * 
+	 * 5. Aim to write the smallest working program possible: increases your
+	 * chances of finishing the project. You can always expand and/or refine it
+	 * later.
+	 * 
+	 * 4. Google like crazy: especially line errors - learn from others'
+	 * mistakes and discoveries
+	 * 
+	 * 3. Build programs one step at a time: always check your program as you go
+	 * so you can fix errors/make improvements bit by bit and not have a huge
+	 * headache just when you think you've finished.
+	 * 
+	 * 2. Ensure braces always pair up: this should be the first thing you check
+	 * if things go wonky in your program.
+	 * 
+	 * 1. Format code correctly: Cntrl+Shift+F! Makes code readable and more
+	 * easy to see mistakes!
+	 * 
+	 */
+}
